cb0a7489132960185e7d81f73d13f545b3976aae
krisrita <ljn_love_life@163.com>
2014-03-17 12:48:52 +0800

    change file

---
+++ Public/Js/normal.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+$(function(){
+	$(".mo").each(function(){
+		$(this).mouseover(function(){
+			$(this).addClass("over");
+		}).mouseout(function(){
+			$(this).removeClass("over");
+		});
+	});
+	$(".tab").each(function(){
+		$(this).mousedown(function(){
+			$(this).addClass("sel").siblings().removeClass("sel");
+		});
+	});
+	$(".clk").each(function(){
+		$(this).mousedown(function(){
+			$(this).addClass("sel");
+		});
+	});
+	$(".upImg").click(function(){
+		$(this).find(".upImgWindow").toggle();
+	});
+	$(".msgBox dt").mouseover(function(){
+		$(this).find(".userConWindow").show();
+	}).mouseout(function(){
+		$(this).find(".userConWindow").hide();
+	});
+});
